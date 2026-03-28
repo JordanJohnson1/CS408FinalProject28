21e83ffc9fb94fc727110067c16f7ccc481529be
Jordan Johnson <JordanJohnson23@u.boisestate.edu>
2026-03-28 16:00:26 -0600

    checkpoint 2

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+  def landing; end
+
+  def play; end
+
+  def shop; end
+
+  def leaderboard; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get "home/index"
+  root "pages#landing"
 
-  # Make "/" go to Hello World page
-  root "home#index"
+  get "/play", to: "pages#play"
+  get "/shop", to: "pages#shop"
+  get "/leaderboard", to: "pages#leaderboard"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   get "up" => "rails/health#show", as: :rails_health_check
